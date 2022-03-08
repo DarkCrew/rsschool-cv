13e07d00942b092dcad25c6e5cb72423f4765e07
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-03-08 19:47:20 +0300

    feat: add burger menu

---
+++ index.js
@@ -0,0 +1,21 @@
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerMenuLines = document.querySelector('.menu-icon');
+const navigationMenu = document.querySelector('.header-nav');
+const welcomeMenuItems = document.querySelectorAll('.header-nav ul li');
+
+burgerMenu.addEventListener('click', changeStateWelcomeMenu);
+
+function changeStateWelcomeMenu() {
+    burgerMenuLines.classList.toggle('active');
+    navigationMenu.classList.toggle('mobile');
+    burgerMenu.classList.toggle('active');
+    document.body.style.overflow = 'hidden';
+}
+
+if(window.innerWidth <= 1024){
+    for (let i = 0; i < welcomeMenuItems.length; i++){
+      welcomeMenuItems[i].addEventListener('click', () => {
+        changeStateWelcomeMenu();
+      });
+    };
+} 
